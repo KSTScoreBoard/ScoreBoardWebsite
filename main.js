fe727b8ad76ea7c5d5705c4847c3908a619bd8d4
KSTScoreBoard <kst.scoreboard@gmail.com>
2023-03-25 23:57:42 +0900

    デザイン変更

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-'use strict'
- {
-    var selected = null;
-    var selecterRed = document.getElementById('selecterRed');
-    var selecterBlue = document.getElementById('selecterBlue');
-    var selecterGreen = document.getElementById('selecterGreen');
-    var selecterYellow = document.getElementById('selecterYellow');
-
-    selecterRed.addEventListener('click', () => {
-        selected = selecterRed;
-    })
-
-    selecterBlue.addEventListener('click', () => {
-        selected = selecterBlue;
-    })
-
-    selecterGreen.addEventListener('click', () => {
-        selected = selecterGreen;
-    })
-
-    selecterYellow.addEventListener('click', () => {
-        selected = selecterYellow;
-    })
-
-    document.getElementById('send').addEventListener('click',() =>{
-        console.log(selected.parentNode.textContent);
-    })
+function block1_click(e){
+  var block1_score = document.getElementById('block1_score');
+  var num = Number(block1_score.textContent);
+  const data = e.currentTarget.dataset['index']; // 1が返される
+  switch(data){
+    case '+100':
+      num += 100;
+      break;
+    case '+10':
+      num += 10;
+      break;
+    case '+1':
+      num += 1;
+      break;
+    case '-100':
+      num -= 100;
+      break;
+    case '-10':
+      num -= 10;
+      break;
+    case '-1':
+      num -= 1;
+      break;
+  }
+  if(num > 999) num = 999;
+  if(num < 0) num= 0;
+  block1_score.textContent = String(num).padStart(3,'0');
 }
