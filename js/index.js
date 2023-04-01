0d531f04a72da10b400690350fe00215be219486
KSTScoreBoard <kst.scoreboard@gmail.com>
2023-04-02 04:51:57 +0900

    blurtooth化

--- js/index.js
+++ js/index.js
@@ -1,54 +1,38 @@
 const socket = new WebSocket('wss://cloud.achex.ca/kst-denshikousaku');
-let auth = "";
+const ble = new BlueJelly();
+//メモ　変更を反映させるボタン
 
-socket.addEventListener('open', function (event) {
-  auth = window.prompt("ペアコードを入力してください", "");
-  console.log('open',event)
-  // 事前登録などはなく使用時に好きなものを入れられます。
-  socket.send('{"auth":"sneder"}');
-});
+window.onload = function () {
+  //UUIDの設定
+  ble.setUUID("b1_score", "686a7d86-7cc6-4983-a906-391e2fb5e6e6", "1c798918-cf8c-11ed-afa1-0242ac120002");
+  ble.setUUID("b1_level", "686a7d86-7cc6-4983-a906-391e2fb5e6e6", "21a4f6d0-cf9a-11ed-afa1-0242ac120002");
+  ble.setUUID("b1_enable", "686a7d86-7cc6-4983-a906-391e2fb5e6e6", "84a073e6-d0c0-11ed-afa1-0242ac120002");
 
-socket.addEventListener('message', function (event) {
-  console.log('message',event)
-  const json = JSON.parse(event.data)
-  console.log(json)
-  // auth時のレスポンスもmessageで来るので弾いておきます
-  if (json.auth == 'OK') {
-  return
-  }
-})
+  document.getElementById("b1_connect").addEventListener('click',() => {ble.requestDevice('686a7d86-7cc6-4983-a906-391e2fb5e6e6');});
+}
 
-function send(){
-  var b1_score = document.getElementById('b1_score').textContent;
-  var b1_lv = document.getElementById('b1_lv').value;
-  socket.send(JSON.stringify({"to":auth, "b1_score":b1_score, "b1_lv":b1_lv}));
+ble.onScan = function (deviceName) {
+  document.getElementById('b1_connect').className = "btn btn-lg bg-success m-2 text-light";
+  document.getElementById('b1_connect').textContent = "接続済み";
 }
 
 
-function block1_click(e){
-  var b1_score = Number(document.getElementById('b1_score').textContent);
-  const data = e.currentTarget.dataset['index']; // 1が返される
-  switch(data){
-    case '+100':
-      b1_score += 100;
-      break;
-    case '+10':
-      b1_score += 10;
-      break;
-    case '+1':
-      b1_score += 1;
-      break;
-    case '-100':
-      b1_score -= 100;
-      break;
-    case '-10':
-      b1_score -= 10;
-      break;
-    case '-1':
-      b1_score -= 1;
-      break;
+function b1_score(append){
+  var score = Number(document.getElementById("b1_score").textContent);
+  score += append;
+  if(score > 999) score = 999;
+  if(score < 0) score = 0;
+  document.getElementById('b1_score').textContent = String(score).padStart(3,'0');
+
+  if(ble.bluetoothDevice != null){
+    const view = new DataView(new ArrayBuffer(2));
+    view.setInt16(0,Number(document.getElementById("b1_score").textContent),true);  
+    ble.write('b1_score', new Uint8Array(view.buffer));
   }
-  if(b1_score > 999) b1_score = 999;
-  if(b1_score < 0) b1_score= 0;
-  document.getElementById('b1_score').textContent = String(b1_score).padStart(3,'0');
+}
+
+function b1_level(element){
+  if(ble.bluetoothDevice != null){
+    ble.write('b1_level', new Uint8Array([element.value]));
+  }
 }
