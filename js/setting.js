b5b3a489e2316af2ac2e1424e29abec81aeac5fc
KST電子工作同好会_LED得点掲示板 <98940384+KSTScoreBoard@users.noreply.github.com>
2023-04-27 20:28:30 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -2,65 +2,37 @@
 var colors = ['danger','primary','success','warning'];
 
 window.addEventListener('load', (event) => {
-  console.log(window.localStorage.getItem('block1_name'));
-  
-  document.getElementById('block1_name').value = window.localStorage.getItem('block1_name');
-  document.getElementById('block1_id').value = window.localStorage.getItem('block1_id');
-  elements = document.getElementsByName('block1_colors');
-  elements.item(colors.indexOf(window.localStorage.getItem('block1_color'))).checked = true;
-
-  document.getElementById('block2_name').value = window.localStorage.getItem('block2_name');
-  document.getElementById('block2_id').value = window.localStorage.getItem('block2_id');
-  elements = document.getElementsByName('block2_colors');
-  elements.item(colors.indexOf(window.localStorage.getItem('block2_color'))).checked = true;
-
-  document.getElementById('block3_name').value = window.localStorage.getItem('block3_name');
-  document.getElementById('block3_id').value = window.localStorage.getItem('block3_id');
-  elements = document.getElementsByName('block3_colors');
-  elements.item(colors.indexOf(window.localStorage.getItem('block3_color'))).checked = true;
-
-  document.getElementById('block4_name').value = window.localStorage.getItem('block4_name');
-  document.getElementById('block4_id').value = window.localStorage.getItem('block4_id');
-  elements = document.getElementsByName('block4_colors');
-  elements.item(colors.indexOf(window.localStorage.getItem('block4_color'))).checked = true;
+  if(window.localStorage.getItem('block1') == null){
+    window.localStorage.setItem('block1','{"name":"赤","id":"block1_radio1","color":"block1_red"}');
+    window.localStorage.setItem('block2','{"name":"青","id":"block2_radio2","color":"block2_blue"}');
+    window.localStorage.setItem('block3','{"name":"緑","id":"block3_radio3","color":"block3_green"}');
+    window.localStorage.setItem('block4','{"name":"黄","id":"block4_radio4","color":"block4_yellow"}');
+  }
+  setBlockData('block1');
+  setBlockData('block2');
+  setBlockData('block3');
+  setBlockData('block4');
 });
 
 function set(){
-  window.localStorage.setItem('block1_name',document.getElementById('block1_name').value);
-  window.localStorage.setItem('block1_id',document.getElementById('block1_id').value);
-  elements = document.getElementsByName('block1_colors');
-  for(var i=0;i<elements.length;i++){
-    if(elements.item(i).checked){
-      window.localStorage.setItem('block1_color',colors[i]);
-    }
-  }
-
-  window.localStorage.setItem('block2_name',document.getElementById('block2_name').value);
-  window.localStorage.setItem('block2_id',document.getElementById('block2_id').value);
-  elements = document.getElementsByName('block2_colors');
-  for(var i=0;i<elements.length;i++){
-    if(elements.item(i).checked){
-      window.localStorage.setItem('block2_color',colors[i]);
-    }
-  }
-
-  window.localStorage.setItem('block3_name',document.getElementById('block3_name').value);
-  window.localStorage.setItem('block3_id',document.getElementById('block3_id').value);
-  elements = document.getElementsByName('block3_colors');
-  for(var i=0;i<elements.length;i++){
-    if(elements.item(i).checked){
-      window.localStorage.setItem('block3_color',colors[i]);
-    }
-  }
-
-  window.localStorage.setItem('block4_name',document.getElementById('block4_name').value);
-  window.localStorage.setItem('block4_id',document.getElementById('block4_id').value);
-  elements = document.getElementsByName('block4_colors');
-  for(var i=0;i<elements.length;i++){
-    if(elements.item(i).checked){
-      window.localStorage.setItem('block4_color',colors[i]);
-    }
-  }
-
+  console.log(document.querySelector("#" + target + " input[name='" + target + "_id']:checked").dataset.x);
+  window.localStorage.setItem('block1',JSON.stringify(getBlockData('block1')));
+  window.localStorage.setItem('block2',JSON.stringify(getBlockData('block2')));
+  window.localStorage.setItem('block3',JSON.stringify(getBlockData('block3')));
+  window.localStorage.setItem('block4',JSON.stringify(getBlockData('block4')));
   alert("設定を保存しました");
+}
+
+function getBlockData(target){
+  var json = {"name":document.querySelector("#" + target + " input[name='name']").value,
+              "id":document.querySelector("#" + target + " input[name='" + target + "_id']:checked").id,
+              'color':document.querySelector("#" + target + " input[name='" + target + "_color']:checked").id};
+  return json;
+}
+
+function setBlockData(target){
+  var json = JSON.parse(window.localStorage.getItem(target));
+  document.querySelector("#" + target + " input[name='name']").value = json['name'];
+  document.getElementById(json['id']).checked = true;
+  document.getElementById(json['color']).checked = true;
 }
