86412e2efbb0f1772b3b9aa213c20409bc0cbe3e
KSTScoreBoard <kst.scoreboard@gmail.com>
2023-04-27 22:26:29 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', (event) => {
   if(window.localStorage.getItem('block1') == null){
-    window.localStorage.setItem('block1','{"name":"赤","id":"0","color":"0"}');
-    window.localStorage.setItem('block2','{"name":"青","id":"1","color":"1"}');
-    window.localStorage.setItem('block3','{"name":"緑","id":"2","color":"2"}');
-    window.localStorage.setItem('block4','{"name":"黄","id":"3","color":"3"}');
+    window.localStorage.setItem('block1','{"name":"赤","to":"1","color":"1"}');
+    window.localStorage.setItem('block2','{"name":"青","to":"2","color":"2"}');
+    window.localStorage.setItem('block3','{"name":"緑","to":"3","color":"3"}');
+    window.localStorage.setItem('block4','{"name":"黄","to":"4","color":"4"}');
   }
   readBlockConfig('block1');
   readBlockConfig('block2');
@@ -13,39 +13,39 @@ window.addEventListener('load', (event) => {
 });
 
 function set(){
-  window.localStorage.setItem('block1',JSON.stringify(writeBlockConfig('block1')));
-  window.localStorage.setItem('block2',JSON.stringify(writeBlockConfig('block2')));
-  window.localStorage.setItem('block3',JSON.stringify(writeBlockConfig('block3')));
-  window.localStorage.setItem('block4',JSON.stringify(writeBlockConfig('block4')));
+  writeBlockConfig('block1');
+  writeBlockConfig('block2');
+  writeBlockConfig('block3');
+  writeBlockConfig('block4');
   alert("設定を保存しました");
 }
 
 function writeBlockConfig(target){
-  var id;
+  var to;
   var color;
 
-  var id_elements = document.querySelectorAll("#" + target + " input[name='" + target + "_id']");
+  var to_elements = document.querySelectorAll("#" + target + " input[name='" + target + "_to']");
   var color_elements = document.querySelectorAll("#" + target + " input[name='" + target + "_color']");
 
-  id_elements.forEach(element => {
+  to_elements.forEach(element => {
     if(element.checked){
-      id = [].slice.call(id_elements).indexOf(element);
+      to = [].slice.call(to_elements).indexOf(element) + 1;
     }
   });
 
   color_elements.forEach(element => {
     if(element.checked){
-      color = [].slice.call(color_elements).indexOf(element);
+      color = [].slice.call(color_elements).indexOf(element) + 1;
     }
   });
 
-  var json = {"name":document.querySelector("#" + target + " input[name='name']").value,"id":id,'color':color};
-  return json;
+  var json = {"name":document.querySelector("#" + target + " input[name='name']").value,"to":to,'color':color};
+  window.localStorage.setItem(target,JSON.stringify(json));
 }
 
 function readBlockConfig(target){
   var json = JSON.parse(window.localStorage.getItem(target));
-  document.querySelector("#" + target + " input[name='name']").value = json['name'];
-  document.querySelectorAll("#" + target + " input[name='" + target + "_id']")[json['id']].checked = true;
-  document.querySelectorAll("#" + target + " input[name='" + target + "_color']")[json['color']].checked = true;
+  document.querySelector("#" + target + " input[name='name']").value = json.name;
+  document.querySelectorAll("#" + target + " input[name='" + target + "_to']")[json.to - 1].checked = true;
+  document.querySelectorAll("#" + target + " input[name='" + target + "_color']")[json.color - 1].checked = true;
 }
