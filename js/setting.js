f61ca401925230c9acf86e5a6c7d371d54710fcd
KSTScoreBoard <kst.scoreboard@gmail.com>
2023-04-27 21:06:47 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,21 +1,18 @@
-
-var colors = ['danger','primary','success','warning'];
-
 window.addEventListener('load', (event) => {
   if(window.localStorage.getItem('block1') == null){
-    window.localStorage.setItem('block1','{"name":"赤","id":"block1_radio1","color":"block1_red"}');
-    window.localStorage.setItem('block2','{"name":"青","id":"block2_radio2","color":"block2_blue"}');
-    window.localStorage.setItem('block3','{"name":"緑","id":"block3_radio3","color":"block3_green"}');
-    window.localStorage.setItem('block4','{"name":"黄","id":"block4_radio4","color":"block4_yellow"}');
+    window.localStorage.setItem('block1','{"name":"赤","id":"0","color":"0"}');
+    window.localStorage.setItem('block2','{"name":"青","id":"1","color":"1"}');
+    window.localStorage.setItem('block3','{"name":"緑","id":"2","color":"2"}');
+    window.localStorage.setItem('block4','{"name":"黄","id":"3","color":"3"}');
   }
   setBlockData('block1');
   setBlockData('block2');
   setBlockData('block3');
   setBlockData('block4');
+
 });
 
 function set(){
-  console.log(document.querySelector("#" + target + " input[name='" + target + "_id']:checked").dataset.x);
   window.localStorage.setItem('block1',JSON.stringify(getBlockData('block1')));
   window.localStorage.setItem('block2',JSON.stringify(getBlockData('block2')));
   window.localStorage.setItem('block3',JSON.stringify(getBlockData('block3')));
@@ -24,15 +21,31 @@ function set(){
 }
 
 function getBlockData(target){
-  var json = {"name":document.querySelector("#" + target + " input[name='name']").value,
-              "id":document.querySelector("#" + target + " input[name='" + target + "_id']:checked").id,
-              'color':document.querySelector("#" + target + " input[name='" + target + "_color']:checked").id};
+  var id;
+  var color;
+
+  var id_elements = document.querySelectorAll("#" + target + " input[name='" + target + "_id']");
+  var color_elements = document.querySelectorAll("#" + target + " input[name='" + target + "_color']");
+
+  id_elements.forEach(element => {
+    if(element.checked){
+      id = [].slice.call(id_elements).indexOf(element);
+    }
+  });
+
+  color_elements.forEach(element => {
+    if(element.checked){
+      color = [].slice.call(color_elements).indexOf(element);
+    }
+  });
+
+  var json = {"name":document.querySelector("#" + target + " input[name='name']").value,"id":id,'color':color};
   return json;
 }
 
 function setBlockData(target){
   var json = JSON.parse(window.localStorage.getItem(target));
   document.querySelector("#" + target + " input[name='name']").value = json['name'];
-  document.getElementById(json['id']).checked = true;
-  document.getElementById(json['color']).checked = true;
+  document.querySelectorAll("#" + target + " input[name='" + target + "_id']")[json['id']].checked = true;
+  document.querySelectorAll("#" + target + " input[name='" + target + "_color']")[json['color']].checked = true;
 }
